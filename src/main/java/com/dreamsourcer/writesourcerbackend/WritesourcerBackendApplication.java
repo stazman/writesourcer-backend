299ac86050c3cb3bc7d27aeaa3a667d9107395fa
Christopher DiStasio <cadistasio@gmail.com>
2021-04-23 16:17:34 -0500

    Initial commit

---
+++ src/main/java/com/dreamsourcer/writesourcerbackend/WritesourcerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dreamsourcer.writesourcerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritesourcerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritesourcerBackendApplication.class, args);
+	}
+
+}
