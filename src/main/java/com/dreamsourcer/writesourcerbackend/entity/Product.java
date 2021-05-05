0ba078c9f9e9b856438f6fe541887ec01b028f6f
Christopher DiStasio <cadistasio@gmail.com>
2021-05-05 16:39:30 -0500

    Creates, stubs Product entity

---
+++ src/main/java/com/dreamsourcer/writesourcerbackend/entity/Product.java
@@ -0,0 +1,58 @@
+package com.dreamsourcer.writesourcerbackend.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data
+//@Getter
+//@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "image_url")
+    private String imageURL;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "date_updated")
+    @UpdateTimestamp
+    private Date dateUpdated;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+
+
+}
