f5e59ed9da58a8171e2dc19c9f485f0a597c1658
Christopher DiStasio <cadistasio@gmail.com>
2021-05-08 22:39:57 -0500

    Adds annotations, association with product

--- src/main/java/com/dreamsourcer/writesourcerbackend/entity/ProductCategory.java
+++ src/main/java/com/dreamsourcer/writesourcerbackend/entity/ProductCategory.java
@@ -9,21 +9,21 @@ import java.util.Set;
 
 @Entity
 @Table(name="product_category")
-@Data
+//@Data -- known Lombok bug
 @Getter
 @Setter
 public class ProductCategory {
 
-    @Column(name="id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    @Column(name="category_name")
+    @Column(name = "category_name")
     private String categoryName;
 
-    @OneToMany(cascade= CascadeType.ALL, mappedBy = "category")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
-
-
 
 }
 
