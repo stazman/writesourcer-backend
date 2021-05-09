ce1c3eccf1af35ec2635163817ee1ced0a387769
Christopher DiStasio <cadistasio@gmail.com>
2021-05-08 22:41:28 -0500

    Updates db name

--- src/main/java/com/dreamsourcer/writesourcerbackend/dao/ProductRepository.java
+++ src/main/java/com/dreamsourcer/writesourcerbackend/dao/ProductRepository.java
@@ -1,9 +1,8 @@
 package com.dreamsourcer.writesourcerbackend.dao;
 
+import com.dreamsourcer.writesourcerbackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-
 
 }
